d61cc1faa625760d7f7014d4d90792a88099f793
Garrett Snyder <garrettsnyder0@gmail.com>
2019-03-15 09:15:39 -0700

    Score calculation and results display complete.  Next step: link to database

--- MentalHealthASP/Controllers/HomeController.cs
+++ MentalHealthASP/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MentalHealthASP.Models;
 
 namespace MentalHealthASP.Controllers
 {
@@ -14,14 +15,36 @@ namespace MentalHealthASP.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult DepressionTest()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult DepressionTest(DepressionTest depressionTest)
+        {
+            depressionTest.testScore = depressionTest.question1 + depressionTest.question2 + depressionTest.question3 +
+                                    depressionTest.question4 + depressionTest.question5 + depressionTest.question6 +
+                                    depressionTest.question7 + depressionTest.question8 + depressionTest.question9;
+            depressionTest.dateTaken = System.DateTime.Now;
+            return View("DepressionResults", depressionTest);
+        }
+
+
+        [HttpGet]
         public ActionResult AnxietyTest()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AnxietyTest(AnxietyTest anxietyTest)
+        {
+            anxietyTest.testScore = anxietyTest.question1 + anxietyTest.question2 + anxietyTest.question3 +
+                                    anxietyTest.question4 + anxietyTest.question5 + anxietyTest.question6 + anxietyTest.question7;
+            anxietyTest.dateTaken = System.DateTime.Now;
+            return View("AnxietyResults", anxietyTest);
         }
 
         public ActionResult History()
--- MentalHealthASP/Models/DepressionTest.cs
+++ MentalHealthASP/Models/DepressionTest.cs
@@ -17,14 +17,14 @@ namespace MentalHealthASP.Models
         public int testScore { get; set; }
         public System.DateTime dateTaken { get; set; }
         public string comments { get; set; }
-        public Nullable<int> question1 { get; set; }
-        public Nullable<int> question2 { get; set; }
-        public Nullable<int> question3 { get; set; }
-        public Nullable<int> question4 { get; set; }
-        public Nullable<int> question5 { get; set; }
-        public Nullable<int> question6 { get; set; }
-        public Nullable<int> question7 { get; set; }
-        public Nullable<int> question8 { get; set; }
-        public Nullable<int> question9 { get; set; }
+        public int question1 { get; set; }
+        public int question2 { get; set; }
+        public int question3 { get; set; }
+        public int question4 { get; set; }
+        public int question5 { get; set; }
+        public int question6 { get; set; }
+        public int question7 { get; set; }
+        public int question8 { get; set; }
+        public int question9 { get; set; }
     }
 }
